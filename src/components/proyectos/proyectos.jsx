bc0de2648c9ab4810f3034910c1e6aa8d50dbb01
Agustin-Lovera04 <agusslovera04@gmail.com>
2024-09-16 17:57:22 -0300

    detalles post deploy

--- src/components/proyectos/proyectos.jsx
+++ src/components/proyectos/proyectos.jsx
@@ -2,6 +2,7 @@ import rugbyShopREACT from '/images/rugbyShopREACT.png';
 import logoÑaku from '/images/logo ñacu.png';
 import rugbyShopJS from '/images/rugbyShopJS.png';
 import logoBackEnd from '/images/logoBackEnd.png';
+import logoBzPrint from '/images/BZPrint-Logo.png'
 import './proyectos.css';
 import { useEffect } from 'react';
 import Aos from 'aos';
@@ -26,6 +27,7 @@ const Proyectos = () => {
                             <a href="https://agustin-lovera04.github.io/PF-Lovera/" target='_blank' rel="noopener noreferrer">
                                 <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al proyecto</button>
                             </a>
+                            <h1 className='title--Card text-danger p-4'>PROYECTO DE PRUEBA EDUCACIONAL</h1>
                         </div>
                     </div>
                 </div>
@@ -40,6 +42,7 @@ const Proyectos = () => {
                             <a href="https://agustin-lovera04.github.io/ProyectoFInal-Lovera/" target='_blank' rel="noopener noreferrer">
                                 <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al proyecto</button>
                             </a>
+                            <h1 className='title--Card text-danger p-4'>PROYECTO DE PRUEBA EDUCACIONAL</h1>
                         </div>
                     </div>
                 </div>
@@ -54,6 +57,7 @@ const Proyectos = () => {
                             <a href="https://agustin-lovera04.github.io/ProyectoFinal-Lovera-React/" target='_blank' rel="noopener noreferrer">
                                 <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al proyecto</button>
                             </a>
+                            <h1 className='title--Card text-danger p-4'>PROYECTO DE PRUEBA EDUCACIONAL</h1>
                         </div>
                     </div>
                 </div>
@@ -66,6 +70,21 @@ const Proyectos = () => {
                                 <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al repositorio</button>
                             </a>
                             <a href="https://proyectofinal-backend-lovera.onrender.com" target='_blank' rel="noopener noreferrer">
+                                <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al proyecto</button>
+                            </a>
+                            <h1 className='title--Card text-danger p-4'>PROYECTO DE PRUEBA EDUCACIONAL</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="card" data-aos="flip-left" data-aos-duration="1800">
+                    <div className="card2">
+                        <img className='img--Card' src={logoBzPrint} alt="Proyecto BZ PRINT" />
+                        <h1 className='title--Card text-light p-4'>BZ PRINT</h1>
+                        <div className="btn--card__Proyectos">
+                            <a href="https://github.com/Agustin-Lovera04/BZ-PRINT" target='_blank' rel="noopener noreferrer">
+                                <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al repositorio</button>
+                            </a>
+                            <a href="https://agustin-lovera04.github.io/BZ-PRINT" target='_blank' rel="noopener noreferrer">
                                 <button className="btn btn-outline-primary m-1 btn-card_Proyectos">Ir al proyecto</button>
                             </a>
                         </div>
